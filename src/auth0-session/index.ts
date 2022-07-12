732c2c2ed9c03c189dda1906a87b60c3111c6c27
James Porcelli <jporcelli1120@gmail.com>
2022-07-12 18:26:35 -0400

    Fix isolated modules export type errors

--- src/auth0-session/index.ts
+++ src/auth0-session/index.ts
@@ -1,9 +1,21 @@
 export { default as CookieStore } from './cookie-store';
 export { default as TransientStore } from './transient-store';
-export { Config, SessionConfig, CookieConfig, LoginOptions, LogoutOptions, AuthorizationParameters } from './config';
-export { get as getConfig, ConfigParameters, DeepPartial } from './get-config';
-export { default as loginHandler, HandleLogin } from './handlers/login';
-export { default as logoutHandler, HandleLogout } from './handlers/logout';
-export { default as callbackHandler, CallbackOptions, AfterCallback, HandleCallback } from './handlers/callback';
-export { default as clientFactory, ClientFactory } from './client';
-export { SessionCache } from './session-cache';
+export type {
+  Config,
+  SessionConfig,
+  CookieConfig,
+  LoginOptions,
+  LogoutOptions,
+  AuthorizationParameters
+} from './config';
+export { get as getConfig } from './get-config';
+export type { ConfigParameters, DeepPartial } from './get-config';
+export { default as loginHandler } from './handlers/login';
+export type { HandleLogin } from './handlers/login';
+export { default as logoutHandler } from './handlers/logout';
+export type { HandleLogout } from './handlers/logout';
+export { default as callbackHandler } from './handlers/callback';
+export type { CallbackOptions, AfterCallback, HandleCallback } from './handlers/callback';
+export { default as clientFactory } from './client';
+export type { ClientFactory } from './client';
+export type { SessionCache } from './session-cache';
